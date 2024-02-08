8439131b91dd9d3e20d2cbac01714b215bc8c370
Rifqihtoh <rifqih1945@gmail.com>
2024-02-08 21:38:09 +0700

    created back-end API for case study

--- Back-End/wallet/index.js
+++ Back-End/wallet/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
-
 const app = express();
+app.use(cors());
 const port = 3000;
 
 // Konfigurasi koneksi ke database PostgreSQL
@@ -15,65 +16,15 @@ const pool = new Pool({
 });
 
 app.use(bodyParser.json());
-// SELECT setval('country_seq', 251, false);
-// Fungsi Create (INSERT)
-app.post('/countries', async (req, res) => {
-  const { iso, name, nicename, iso3, numcode, phonecode } = req.body;
-  const query = 'INSERT INTO country(iso, name, nicename, iso3, numcode, phonecode) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
-  const values = [iso, name, nicename, iso3, numcode, phonecode];
-
-  try {
-    const result = await pool.query(query, values);
-    res.json(result.rows[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 // Fungsi Read (SELECT)
-// Fungsi Read (SELECT) dengan Paginasi
-app.get('/countries', async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const offset = (page - 1) * limit;
-
-  const query = 'SELECT * FROM country ORDER BY id OFFSET $1 LIMIT $2';
+app.get('/wallet/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const query = 'SELECT * FROM wallet WHERE id = $1';
   
   try {
-    const result = await pool.query(query, [offset, limit]);
-
-    // Menghitung total entri tanpa paginasi
-    const totalQuery = 'SELECT COUNT(*) FROM country';
-    const totalResult = await pool.query(totalQuery);
-    const totalEntries = parseInt(totalResult.rows[0].count, 10);
-
-    const totalPages = Math.ceil(totalEntries / limit);
-
-    res.json({
-      data: result.rows,
-      pagination: {
-        page: parseInt(page, 10),
-        limit: parseInt(limit, 10),
-        totalEntries,
-        totalPages,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Fungsi Update (UPDATE)
-app.put('/countries/:id', async (req, res) => {
-  const countryId = req.params.id;
-  const { iso, name, nicename, iso3, numcode, phonecode } = req.body;
-  const query = 'UPDATE country SET iso=$1, name=$2, nicename=$3, iso3=$4, numcode=$5, phonecode=$6 WHERE id=$7 RETURNING *';
-  const values = [iso, name, nicename, iso3, numcode, phonecode, countryId];
-
-  try {
-    const result = await pool.query(query, values);
-    res.json(result.rows[0]);
+    const result = await pool.query(query, [id]);
+    res.json(result.rows);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
@@ -81,19 +32,16 @@ app.put('/countries/:id', async (req, res) => {
 });
 
 // Fungsi Patch (Partial Update)
-app.patch('/countries/:id', async (req, res) => {
-  const countryId = req.params.id;
+app.patch('/wallet/:id', async (req, res) => {
+  const Id = req.params.id;
+  const balance = req.body.balance;
   const updateFields = req.body;
 
   if (Object.keys(updateFields).length === 0) {
     return res.status(400).send('Bad Request: No fields to update');
   }
-
-  const setClause = Object.keys(updateFields).map((key, index) => `${key}=$${index + 1}`).join(', ');
-  const values = Object.values(updateFields);
-  values.push(countryId);
-
-  const query = `UPDATE country SET ${setClause} WHERE id=$${values.length} RETURNING *`;
+  const query = 'UPDATE wallet SET balance = balance + $1 WHERE id=$2 RETURNING *';
+  const values = [balance, Id];
 
   try {
     const result = await pool.query(query, values);
@@ -104,20 +52,19 @@ app.patch('/countries/:id', async (req, res) => {
   }
 });
 
-// Fungsi Delete (DELETE)
-app.delete('/countries/:id', async (req, res) => {
-  const countryId = req.params.id;
-  const query = 'DELETE FROM country WHERE id=$1 RETURNING *';
+// Fungsi Create (INSERT)
+app.post('/wallet/:id', async (req, res) => {
+  const Id = req.params.id;
+  const balance = req.body.balance;
+  const query = 'INSERT into transaction (wallet_id, name, description, amount, type) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
+  const {name = "Top-Up", description = "Top-Up Saldo", amount = balance, type = "IN"} = req.body
+  const values = [Id, name, description, amount, type];
 
   try {
-    const result = await pool.query(query, [countryId]);
+    const result = await pool.query(query, values);
     res.json(result.rows[0]);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
 });
