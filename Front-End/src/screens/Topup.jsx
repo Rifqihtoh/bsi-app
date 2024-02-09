246cc9192dba9b13535fb4f984c0effc7e651e98
Rifqihtoh <rifqih1945@gmail.com>
2024-02-09 09:57:41 +0700

    Succesfully integrate front-end and back end

--- Front-End/src/screens/Topup.jsx
+++ Front-End/src/screens/Topup.jsx
@@ -1,33 +1,81 @@
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator } from "react-native";
+import axios from "axios";
 import React, { useState } from "react";
 
 export default Topup = ({ route, navigation }) => {
-    const [amountTemp, setAmountTemp] = useState(0);
-    const [amount, setAmount] = useState(amountTemp);
-    const {id} = route.params;
-    console.log({id});
-    console.log({amount});
+  const [amount, setAmount] = useState(0);
+  const { id } = route.params;
+  const [loadingAxios, setLoadingAxios] = useState("");
+  const [resultAxios, setResultAxios] = useState("");
+  console.log({ id });
+  console.log({ amount });
+
+  const handleSubmit = async () => {
+    submitWithAxios()
+    .then(submitPatchAxios())
+    .then(() => navigation.navigate("Landing"))
+    .catch((error) => console.error(error));
+  }
+  const submitPatchAxios = async () => {
+    try {
+      setAmount(amount);
+      const payload = {
+        balance: amount,
+      };
+      setLoadingAxios(true);
+      const response = await axios({
+        baseURL: "http://localhost:3000",
+        url: "/wallet/" + id,
+        method: "PATCH",
+        data: payload,
+      });
+      setLoadingAxios(false);
+      setResultAxios("Top-Up Successful!");
+      return response;
+    } catch (error) {
+      setLoadingAxios(false);
+      console.error(error);
+    }
+  }
+  const submitWithAxios = async () => {
+    try {
+      setAmount(amount);
+      const payload = {
+        amount: amount,
+      };
+      setLoadingAxios(true);
+      const response = await axios({
+        baseURL: "http://localhost:3000",
+        url: "/wallet/" + id,
+        method: "POST",
+        data: payload,
+      });
+      setLoadingAxios(false);
+      return response;
+    } catch (error) {
+      setLoadingAxios(false);
+      console.error(error);
+    }
+  };
 
   return (
     <View style={{ flex: 1, padding: 20, marginTop: 5, gap: 15 }}>
       <Text style={{ fontWeight: "bold" }}>Nilai Top-up</Text>
       <TextInput
         style={styles.input}
-        onChangeText={(text) => setAmountTemp(text)}
-        placeholder={amountTemp}
+        onChangeText={(text) => setAmount(text)}
+        placeholder={amount}
       ></TextInput>
-      <TouchableOpacity style={styles.button} onPress={() => setAmount(amountTemp)}>
+      {loadingAxios ? (<ActivityIndicator size="large" color="blue" /> ): (<TouchableOpacity
+        style={styles.button}
+        onPress={() => handleSubmit()}
+      >
         <Text style={styles.buttonText}>Topup</Text>
-      </TouchableOpacity>
+      </TouchableOpacity>)}
+      {resultAxios && <Text>{resultAxios}</Text>}
     </View>
   );
-  console.log({amount});
+  console.log({ amount });
 };
 
 const styles = StyleSheet.create({
